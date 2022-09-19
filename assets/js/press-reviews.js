9b642f4b1b6c9cb2b5cc7173b36e16283c77058d
Daniil <tochka098@gmail.com>
2022-09-19 23:40:09 +0300

    added sections, slider

---
+++ assets/js/press-reviews.js
@@ -0,0 +1,41 @@
+let btnNY = document.querySelector(".press__links .press__link");
+let btnCH = document.querySelectorAll(".press__links .press__link")[1];
+let btnBI = document.querySelectorAll(".press__links .press__link")[2];
+
+let contentNY = document.querySelector(".press__quotes .press__quote");
+let contentCH = document.querySelectorAll(".press__quotes .press__quote")[1];
+let contentBI = document.querySelectorAll(".press__quotes .press__quote")[2];
+
+let nyFunction = function () {
+  contentNY.className = "press__quote active";
+  contentCH.className = "press__quote";
+  contentBI.className = "press__quote";
+
+  btnNY.className = "press__link active";
+  btnCH.className = "press__link";
+  btnBI.className = "press__link";
+};
+
+let chFunction = function () {
+  contentNY.className = "press__quote";
+  contentCH.className = "press__quote active";
+  contentBI.className = "press__quote";
+
+  btnNY.className = "press__link";
+  btnCH.className = "press__link active";
+  btnBI.className = "press__link";
+};
+
+let biFunction = function () {
+  contentNY.className = "press__quote";
+  contentCH.className = "press__quote";
+  contentBI.className = "press__quote active";
+
+  btnNY.className = "press__link";
+  btnCH.className = "press__link";
+  btnBI.className = "press__link active";
+};
+
+btnNY.onclick = nyFunction;
+btnCH.onclick = chFunction;
+btnBI.onclick = biFunction;
