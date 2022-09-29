b4cbc41c777731288c484aa00c9d99d8c10e9fea
10 <tochka098@gmail.com>
2022-09-29 19:55:48 +0300

    improvements in "press-reviews.js"

--- assets/js/press-reviews.js
+++ assets/js/press-reviews.js
@@ -1,27 +1,12 @@
 const tabLinks = document.querySelectorAll(".press__link");
 const tabQuotes = document.querySelectorAll(".press__quote");
 
-tabLinks.forEach(function (el) {
-  el.addEventListener("click", openTabs);
+tabLinks.forEach((el, index) => {
+  el.addEventListener("click", function openTab() {
+    tabLinks.forEach((link) => link.classList.remove("active"));
+    tabQuotes.forEach((quote) => quote.classList.remove("active"));
+
+    el.classList.add("active");
+    tabQuotes[index].classList.add("active");
+  });
 });
-
-function openTabs(el) {
-  const linkTarget = el.currentTarget;
-  const company = linkTarget.dataset.company;
-
-  // Remove class .acvite for all links
-  tabLinks.forEach(function (el) {
-    el.classList.remove("active");
-  });
-
-  // Remove class .acvite for all quotes
-  tabQuotes.forEach(function (el) {
-    el.classList.remove("active");
-  });
-
-  // Add class .active to the targeted link
-  linkTarget.classList.add("active");
-
-  // Add class .active to element with the same id as the link's data-attribute
-  document.getElementById(company).classList.add("active");
-}
