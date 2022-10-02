515aa69f6c160ec2c121a440668485c156b9a44f
10 <tochka098@gmail.com>
2022-10-02 14:18:22 +0300

    added swiper slider

--- assets/js/swiper-slider.js
+++ assets/js/swiper-slider.js
@@ -1,6 +1,20 @@
-// Initialize
+//tabs for sliders
+const tabs = document.querySelectorAll(".reviews__tab");
+const content = document.querySelectorAll(".reviews__content");
+
+tabs.forEach((el, index) => {
+  el.addEventListener("click", function openTab() {
+    tabs.forEach((link) => link.classList.remove("active"));
+    content.forEach((quote) => quote.classList.remove("active"));
+
+    el.classList.add("active");
+    content[index].classList.add("active");
+  });
+});
+
+// Swiper
 const swiper = new Swiper(".reviews-slider", {
-  mousewheel: {},
+  // mousewheel: {},
   autoplay: {
     delay: 3000,
   },
